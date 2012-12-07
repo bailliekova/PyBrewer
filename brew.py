6cbc6d18515f46133c98d52664ef5553520baf2d
anov <novikova.go.zoom@gmail.com>
2012-12-07 14:47:37 +0000

    minor edits, methods for selector class

--- brew.py
+++ brew.py
@@ -1,6 +1,5 @@
 def hex_to_rgb(value):
     value = value.lstrip('#')
-    #if len(value)!=6: raise Exception("bad input")
     return tuple(int(value[i:i+2], 16) for i in range(0, 6, 2))
 
 def rgb_to_hex(rgb):
@@ -9,11 +8,10 @@ def rgb_to_hex(rgb):
 class Brew:
     def __init__(self, datatype, colors, scheme):
         self.datatype=datatype
-        self.num_bins=len(colors)
         self.colors=tuple(colors)
         self.scheme=scheme
-        self.index=self.num_bins
-    
+        self.num_bins=len(self.colors)
+        
     def hexColors(self):
         return self.colors
     
@@ -21,7 +19,7 @@ class Brew:
         return tuple([hex_to_rgb(color) for color in self.colors])
     
     def __str__(self):
-        return '<Brew object: datatype=%s bins=%s scheme=%s>' % (self.datatype, self.num_bins, self.scheme)
+        return '<Brew object: datatype=%s scheme=%s>' % (self.datatype, self.scheme)
     
     def genHexColors(self):
         for index in range(0, len(self.colors)):
--- selector.py
+++ selector.py
@@ -1,5 +1,5 @@
 from brew import Brew
-
+BuGn=Brew('sequential', ['#F7FCFD', '#E5F5F9', '#CCECE6','#99D8C9', '#66C2A4', '#41AE76', '#238B45', '#006D2C', '#00441B'], 'BuGn')
 
 class BrewSelector:
     def __init__(self):
@@ -7,14 +7,16 @@ class BrewSelector:
            
                     'diverging':[{}], 
            
-                    'categorical':[{}]
+                    'qualitative':[{}]
                     }
 
     def get_Brew(datatype, num_bins, scheme):
         try:
-            return self.brews[datatype][num_bins-3][scheme]
+            return self.brews[datatype][scheme]
         except KeyError:
             raise Exception("There is no such Brew, sorry.")
+        except IndexError:
+            raise Exception("There are no Brews for that many bins for that datatype")
     
     def list_brew_schemes(datatype, num_bins):
         try:
