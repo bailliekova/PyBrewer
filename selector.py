4bcc34821b54ce1b23c118b2af71eb108708b3c0
anov <novikova.go.zoom@gmail.com>
2013-01-18 23:11:49 -0600

    revised data structure of brew selector

--- selector.py
+++ selector.py
@@ -1,26 +1,24 @@
 from brew import Brew
 BuGn=Brew('sequential', ['#F7FCFD', '#E5F5F9', '#CCECE6','#99D8C9', '#66C2A4', '#41AE76', '#238B45', '#006D2C', '#00441B'], 'BuGn')
 
+
 class BrewSelector:
     def __init__(self):
-        self.brews={'sequential':[{}], 
+        self.brews={'sequential': {9:{'BuGn': BuGn}}, 
            
-                    'diverging':[{}], 
+                    'diverging': {9:{'RdGn': BuGn}}, 
            
-                    'qualitative':[{}]
+                    'qualitative': {9:{'Colors':BuGn}},
                     }
 
-    def get_Brew(datatype, num_bins, scheme):
+    def get_brew(self, datatype, num_bins, scheme):
         try:
-            return self.brews[datatype][scheme]
+            return self.brews[datatype][num_bins][scheme]
         except KeyError:
             raise Exception("There is no such Brew, sorry.")
-        except IndexError:
-            raise Exception("There are no Brews for that many bins for that datatype")
     
-    def list_brew_schemes(datatype, num_bins):
+    def list_brew_schemes(self, datatype, num_bins):
         try:
-            return tuple(brew.scheme for brew in self.brews[datatype][num_bins-3].values())
-        except IndexError:
+            return tuple(brew.scheme for brew in self.brews[datatype][num_bins].values())
+        except KeyError:
             raise Exception("There are no Brews for that many bins for that datatype")
-        
