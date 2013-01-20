4d738062ef67f1736a77fd520802b3b5450ed595
anov <novikova.go.zoom@gmail.com>
2013-01-19 23:45:34 -0600

    added colorblind/photocopy/print safe attributes
    Also added BuGn sequential brews.

--- brew.py
+++ brew.py
@@ -6,16 +6,19 @@ def rgb_to_hex(rgb):
     return '#%X%X%X' % rgb
 
 class Brew:
-    def __init__(self, datatype, colors, scheme):
+    def __init__(self, datatype, colors, scheme, colorblind_safe=False, photocopy_safe=False, print_friendly=False):
         self.datatype=datatype
         self.colors=tuple(colors)
         self.scheme=scheme
         self.num_bins=len(self.colors)
+        self.colorblind_safe=colorblind_safe
+        self.photocopy_safe=photocopy_safe
+        self.print_friendly=print_friendly
         
     def hexColors(self):
         return self.colors
     
-    def rgbColorTuples(self):
+    def rgbColors(self):
         return tuple([hex_to_rgb(color) for color in self.colors])
     
     def __str__(self):
@@ -24,5 +27,3 @@ class Brew:
     def genHexColors(self):
         for index in range(0, len(self.colors)):
             yield self.colors[index]
-
-
--- selector.py
+++ selector.py
@@ -1,15 +1,34 @@
 from brew import Brew
-BuGn=Brew('sequential', ['#F7FCFD', '#E5F5F9', '#CCECE6','#99D8C9', '#66C2A4', '#41AE76', '#238B45', '#006D2C', '#00441B'], 'BuGn')
+
+BuGn9=Brew('sequential', ['#F7FCFD', '#E5F5F9', '#CCECE6','#99D8C9', '#66C2A4', '#41AE76', '#238B45', '#006D2C', '#00441B'], 'BuGn', colorblind_safe=True)
+BuGn8=Brew('sequential', ['#F7FCFD', '#E5F5F9', '#CCECE6','#99D8C9', '#66C2A4', '#41AE76', '#238B45', '#005824'], 'BuGn', colorblind_safe=True)
+BuGn7=Brew('sequential', ['#EDF8FB', '#CCECE6', '#99D8C9', '#66C2A4', '#41AE76', '#238B45', '#005824'], 'BuGn', colorblind_safe=True)
+BuGn6=Brew('sequential', ['#EDF8FB'. '#CCECE6', '#99D8C9', '#66C2A4', '#2CA25F', '#006D2C'], 'BuGn', colorblind_safe=True)
+BuGn5=Brew('sequential', ['#EDF8FB', '#B2E2E2', '#66C2A4', '#2CA25F', '#006D2C'], 'BuGn', colorblind_safe=True)
+BuGn4=Brew('sequential', ['#EDF8FB', '#B2E2E2', '#66C2A4', '#238B45'], 'BuGn', colorblind_safe=True)
+BuGn3=Brew('sequential', ['#E5F5F9', '#99D8C9', '#2CA25F'], 'BuGn', colorblind_safe=True)
 
 
 class BrewSelector:
     def __init__(self):
-        self.brews={'sequential': {9:{'BuGn': BuGn}}, 
+        self.brews={'sequential': {
+                        3:{'BuGn': BuGn3},
+                        4:{'BuGn': BuGn4},
+                        5:{'BuGn': BuGn5},
+                        6:{'BuGn': BuGn6},
+                        7:{'BuGn': BuGn7},
+                        8:{'BuGn': BuGn8},
+                        9:{'BuGn': BuGn9}
+                    }, 
            
-                    'diverging': {9:{'RdGn': BuGn}}, 
+                    'diverging': {
+                        9:{'RdGn': BuGn,}
+                    }, 
            
-                    'qualitative': {9:{'Colors':BuGn}},
-                    }
+                    'qualitative': {
+                        9:{'Colors':BuGn},
+                    },
+                }
 
     def get_brew(self, datatype, num_bins, scheme):
         try:
