d98a701f55fc7f5425d5fdb131c2dc6c5ce349ef
anov <novikova.go.zoom@gmail.com>
2013-01-21 09:37:21 -0600

    added all qualitative brews, updated tests
    Woo hoo!

--- selector.py
+++ selector.py
@@ -109,6 +109,133 @@ YlOrRd5=Brew('sequential', sequential_from_scheme(YlOrRd, 5), 'YlOrRd', colorbli
 YlOrRd4=Brew('sequential', sequential_from_scheme(YlOrRd, 4), 'YlOrRd', colorblind_safe=True, print_safe=True)
 YlOrRd3=Brew('sequential', sequential_from_scheme(YlOrRd, 3), 'YlOrRd', colorblind_safe=True, photocopy_safe=True, print_safe=True)
 
+Blues=['#F7FBFF', '#', '#DEEBF7', '#C6DBEF', '#BDD7E7', '#9ECAE1', '#6BAED6', '#4292C6', '#3182BD', '#2171B5', '#08519C', '#084594', '#08306B']
+Blues9=Brew('sequential', sequential_from_scheme(Blues, 9), 'Blues', colorblind_safe=True)
+Blues8=Brew('sequential', sequential_from_scheme(Blues, 8), 'Blues', colorblind_safe=True)
+Blues7=Brew('sequential', sequential_from_scheme(Blues, 7), 'Blues', colorblind_safe=True)
+Blues6=Brew('sequential', sequential_from_scheme(Blues, 6), 'Blues', colorblind_safe=True)
+Blues5=Brew('sequential', sequential_from_scheme(Blues, 5), 'Blues', colorblind_safe=True)
+Blues4=Brew('sequential', sequential_from_scheme(Blues, 4), 'Blues', colorblind_safe=True)
+Blues3=Brew('sequential', sequential_from_scheme(Blues, 3), 'Blues', colorblind_safe=True, photocopy_safe=True, print_safe=True)
+
+Greens=['#F7FCF5', '#EDF8E9', '#E5F5E0', '#C7E9C0', '#BAE4B3', '#A2D99B', '#74C476', '#41AB5D', '#31A354', '#238B45', '#006D2C', '#005A32', '#00441B']
+Greens9=Brew('sequential', sequential_from_scheme(Greens, 9), 'Greens', colorblind_safe=True)
+Greens8=Brew('sequential', sequential_from_scheme(Greens, 8), 'Greens', colorblind_safe=True)
+Greens7=Brew('sequential', sequential_from_scheme(Greens, 7), 'Greens', colorblind_safe=True)
+Greens6=Brew('sequential', sequential_from_scheme(Greens, 6), 'Greens', colorblind_safe=True)
+Greens5=Brew('sequential', sequential_from_scheme(Greens, 5), 'Greens', colorblind_safe=True)
+Greens4=Brew('sequential', sequential_from_scheme(Greens, 4), 'Greens', colorblind_safe=True)
+Greens3=Brew('sequential', sequential_from_scheme(Greens, 3), 'Greens', colorblind_safe=True, photocopy_safe=True, print_safe=True)
+
+Greys=['#FFFFFF', '#F7F7F7', '#F0F0F0', '#D9D9D9', '#CCCCCC', '#BDBDBD', '#969696', '#737373', '#636363', '#525252', '#252525', '#252525', '#000000']
+Greys9=Brew('sequential', sequential_from_scheme(Greys, 9), 'Greys', colorblind_safe=True)
+Greys8=Brew('sequential', sequential_from_scheme(Greys, 8), 'Greys', colorblind_safe=True)
+Greys7=Brew('sequential', sequential_from_scheme(Greys, 7), 'Greys', colorblind_safe=True)
+Greys6=Brew('sequential', sequential_from_scheme(Greys, 6), 'Greys', colorblind_safe=True)
+Greys5=Brew('sequential', sequential_from_scheme(Greys, 5), 'Greys', colorblind_safe=True)
+Greys4=Brew('sequential', sequential_from_scheme(Greys, 4), 'Greys', colorblind_safe=True, print_safe=True)
+Greys3=Brew('sequential', sequential_from_scheme(Greys, 3), 'Greys', colorblind_safe=True, photocopy_safe=True, print_safe=True)
+
+Oranges=['#FFF5EB', '#', '#FEE6CE', '#FDD0A2', '#', '#FDAE6B', '#FD8D3C', '#F16913', '#','#D94801', '#A63603', '#8C2D04', '#7F2704']
+Oranges9=Brew('sequential', sequential_from_scheme(Oranges, 9), 'Oranges', colorblind_safe=True)
+Oranges8=Brew('sequential', sequential_from_scheme(Oranges, 8), 'Oranges', colorblind_safe=True)
+Oranges7=Brew('sequential', sequential_from_scheme(Oranges, 7), 'Oranges', colorblind_safe=True)
+Oranges6=Brew('sequential', sequential_from_scheme(Oranges, 6), 'Oranges', colorblind_safe=True)
+Oranges5=Brew('sequential', sequential_from_scheme(Oranges, 5), 'Oranges', colorblind_safe=True)
+Oranges4=Brew('sequential', sequential_from_scheme(Oranges, 4), 'Oranges', colorblind_safe=True)
+Oranges3=Brew('sequential', sequential_from_scheme(Oranges, 3), 'Oranges', colorblind_safe=True, photocopy_safe=True, print_safe=True)
+
+Purples=['#FCFBFD', '#F2F0F7', '#EFEDF5', '#DADAEB', '#CBC9E2', '#BCBDDC', '#9E9AC8', '#807DBA', '#756BB1', '#6A51A3', '#54278F', '#4A1486', '#3F007D']
+Purples9=Brew('sequential', sequential_from_scheme(Purples, 9), 'Purples', colorblind_safe=True)
+Purples8=Brew('sequential', sequential_from_scheme(Purples, 8), 'Purples', colorblind_safe=True)
+Purples7=Brew('sequential', sequential_from_scheme(Purples, 7), 'Purples', colorblind_safe=True)
+Purples6=Brew('sequential', sequential_from_scheme(Purples, 6), 'Purples', colorblind_safe=True)
+Purples5=Brew('sequential', sequential_from_scheme(Purples, 5), 'Purples', colorblind_safe=True)
+Purples4=Brew('sequential', sequential_from_scheme(Purples, 4), 'Purples', colorblind_safe=True)
+Purples3=Brew('sequential', sequential_from_scheme(Purples, 3), 'Purples', colorblind_safe=True, photocopy_safe=True, print_safe=True)
+
+Reds=['#FFF5F0', '#FEE5D9', '#FEE0D2', '#FCBBA1', '#FCAE91', '#FC9272', '#FB6A4A', '#EF3B2C', '#DE2D26', '#CB181D', '#A50F15', '#99000D', '#67000D']
+Reds9=Brew('sequential', sequential_from_scheme(Reds, 9), 'Reds', colorblind_safe=True)
+Reds8=Brew('sequential', sequential_from_scheme(Reds, 8), 'Reds', colorblind_safe=True)
+Reds7=Brew('sequential', sequential_from_scheme(Reds, 7), 'Reds', colorblind_safe=True)
+Reds6=Brew('sequential', sequential_from_scheme(Reds, 6), 'Reds', colorblind_safe=True)
+Reds5=Brew('sequential', sequential_from_scheme(Reds, 5), 'Reds', colorblind_safe=True)
+Reds4=Brew('sequential', sequential_from_scheme(Reds, 4), 'Reds', colorblind_safe=True)
+Reds3=Brew('sequential', sequential_from_scheme(Reds, 3), 'Reds', colorblind_safe=True, photocopy_safe=True, print_safe=True)
+
+Accent=['#7FC97F', '#BEAED4', '#FDC086', '#FFFF99', '#386CB0', '#F0027F', '#BF5B17', '#666666']
+Accent8=Brew('qualitative', qualitative_from_scheme(Accent, 8), 'Accent')
+Accent7=Brew('qualitative', qualitative_from_scheme(Accent, 7), 'Accent')
+Accent6=Brew('qualitative', qualitative_from_scheme(Accent, 6), 'Accent')
+Accent5=Brew('qualitative', qualitative_from_scheme(Accent, 5), 'Accent')
+Accent4=Brew('qualitative', qualitative_from_scheme(Accent, 4), 'Accent', print_safe=True)
+Accent3=Brew('qualitative', qualitative_from_scheme(Accent, 3), 'Accent', print_safe=True)
+
+Dark2=['#1B9E77', '#D95F02', '#7570B3', '#E7298A', '#66A61E', '#E6AB02', '#A6761D', '#666666']
+Dark2_8=Brew('qualitative', qualitative_from_scheme(Dark2, 8), 'Dark2', print_safe=True)
+Dark2_7=Brew('qualitative', qualitative_from_scheme(Dark2, 7), 'Dark2', print_safe=True)
+Dark2_6=Brew('qualitative', qualitative_from_scheme(Dark2, 6), 'Dark2', print_safe=True)
+Dark2_5=Brew('qualitative', qualitative_from_scheme(Dark2, 5), 'Dark2', print_safe=True)
+Dark2_4=Brew('qualitative', qualitative_from_scheme(Dark2, 4), 'Dark2', print_safe=True)
+Dark2_3=Brew('qualitative', qualitative_from_scheme(Dark2, 3), 'Dark2',colorblind_safe=True, print_safe=True)
+
+Paired=['#A6CEE3', '#1F78B4', '#B2DF8A', '#33A02C', '#FB9A99', '#E31A1C', '#FDBF6F', '#FF7F00', '#CAB2D6', '#6A3D9A', '#FFFF99']
+Paired11=Brew('qualitative', qualitative_from_scheme(Paired, 11), 'Paired')
+Paired10=Brew('qualitative', qualitative_from_scheme(Paired, 10), 'Paired')
+Paired9=Brew('qualitative', qualitative_from_scheme(Paired, 9), 'Paired')
+Paired8=Brew('qualitative', qualitative_from_scheme(Paired, 8), 'Paired')
+Paired7=Brew('qualitative', qualitative_from_scheme(Paired, 7), 'Paired', print_safe=True)
+Paired6=Brew('qualitative', qualitative_from_scheme(Paired, 6), 'Paired', print_safe=True)
+Paired5=Brew('qualitative', qualitative_from_scheme(Paired, 5), 'Paired', print_safe=True)
+Paired4=Brew('qualitative', qualitative_from_scheme(Paired, 4), 'Paired', colorblind_safe=True, print_safe=True)
+Paired3=Brew('qualitative', qualitative_from_scheme(Paired, 3), 'Paired', colorblind_safe=True, print_safe=True)
+
+Pastel1=['#FBB4AE', '#B3CDE3', '#CCEBC5', '#DECBE4', '#FED9A6', '#FFFFCC', '#E5D8BD', '#FDDAEC', '#F2F2F2']
+Pastel1_9=Brew('qualitative', qualitative_from_scheme(Pastel1, 9), 'Pastel1')
+Pastel1_8=Brew('qualitative', qualitative_from_scheme(Pastel1, 8), 'Pastel1')
+Pastel1_7=Brew('qualitative', qualitative_from_scheme(Pastel1, 7), 'Pastel1')
+Pastel1_6=Brew('qualitative', qualitative_from_scheme(Pastel1, 6), 'Pastel1')
+Pastel1_5=Brew('qualitative', qualitative_from_scheme(Pastel1, 5), 'Pastel1')
+Pastel1_4=Brew('qualitative', qualitative_from_scheme(Pastel1, 4), 'Pastel1')
+Pastel1_3=Brew('qualitative', qualitative_from_scheme(Pastel1, 3), 'Pastel1')
+
+Pastel2=['#B3E2CD', '#FDCDAC', '#CBD5E8', '#F4CAE4', '#E6F5C9', '#FFF2AE', '#F1E2CC', '#CCCCCC']
+Pastel2_8=Brew('qualitative', qualitative_from_scheme(Pastel2, 8), 'Pastel2')
+Pastel2_7=Brew('qualitative', qualitative_from_scheme(Pastel2, 7), 'Pastel2')
+Pastel2_6=Brew('qualitative', qualitative_from_scheme(Pastel2, 6), 'Pastel2')
+Pastel2_5=Brew('qualitative', qualitative_from_scheme(Pastel2, 5), 'Pastel2')
+Pastel2_4=Brew('qualitative', qualitative_from_scheme(Pastel2, 4), 'Pastel2')
+Pastel2_3=Brew('qualitative', qualitative_from_scheme(Pastel2, 3), 'Pastel2')
+
+Set1=['#E41A1C', '#377EB8', '#4DAF4A', '#984EA3', '#FF7F00', '#FFFF33', '#A65628', '#F781BF', '#999999']
+Set1_9=Brew('qualitative', qualitative_from_scheme(Set1, 9), 'Set1', print_safe=True)
+Set1_8=Brew('qualitative', qualitative_from_scheme(Set1, 8), 'Set1', print_safe=True)
+Set1_7=Brew('qualitative', qualitative_from_scheme(Set1, 7), 'Set1', print_safe=True)
+Set1_6=Brew('qualitative', qualitative_from_scheme(Set1, 6), 'Set1', print_safe=True)
+Set1_5=Brew('qualitative', qualitative_from_scheme(Set1, 5), 'Set1', print_safe=True)
+Set1_4=Brew('qualitative', qualitative_from_scheme(Set1, 4), 'Set1', print_safe=True)
+Set1_3=Brew('qualitative', qualitative_from_scheme(Set1, 3), 'Set1', print_safe=True)
+
+Set2=['#66C2A5', '#FC8D62', '#8DA0CB', '#E78AC3', '#A6D854', '#FFD92F', '#E5C494', '#B3B3b3']
+Set2_8=Brew('qualitative', qualitative_from_scheme(Set2, 8), 'Set2')
+Set2_7=Brew('qualitative', qualitative_from_scheme(Set2, 7), 'Set2')
+Set2_6=Brew('qualitative', qualitative_from_scheme(Set2, 6), 'Set2')
+Set2_5=Brew('qualitative', qualitative_from_scheme(Set2, 5), 'Set2', print_safe=True)
+Set2_4=Brew('qualitative', qualitative_from_scheme(Set2, 4), 'Set2', print_safe=True)
+Set2_3=Brew('qualitative', qualitative_from_scheme(Set2, 3), 'Set2', colorblind_safe=True, print_safe=True)
+
+Set3=['#8DD3C7', '#FFFFB3', '#BEBADA', '#FB8072', '#80B1D3', '#FDB462', '#B3DE69', '#FCCDE5', '#D9D9D9', '#BC80BD', '#CCEBC5', '#FFED6F']
+Set3_12=Brew('qualitative', qualitative_from_scheme(Set3, 12), 'Set3')
+Set3_11=Brew('qualitative', qualitative_from_scheme(Set3, 11), 'Set3')
+Set3_10=Brew('qualitative', qualitative_from_scheme(Set3, 10), 'Set3')
+Set3_9=Brew('qualitative', qualitative_from_scheme(Set3, 9), 'Set3')
+Set3_8=Brew('qualitative', qualitative_from_scheme(Set3, 8), 'Set3', print_safe=True)
+Set3_7=Brew('qualitative', qualitative_from_scheme(Set3, 7), 'Set3', print_safe=True)
+Set3_6=Brew('qualitative', qualitative_from_scheme(Set3, 6), 'Set3', print_safe=True)
+Set3_5=Brew('qualitative', qualitative_from_scheme(Set3, 5), 'Set3', print_safe=True)
+Set3_4=Brew('qualitative', qualitative_from_scheme(Set3, 4), 'Set3', print_safe=True)
+Set3_3=Brew('qualitative', qualitative_from_scheme(Set3, 3), 'Set3', photocopy_safe=True,print_safe=True)
+
 brews=[
     BuGn3, BuGn4, BuGn5, BuGn6, BuGn7, BuGn8, BuGn9, 
     BuPu3, BuPu4, BuPu5, BuPu6, BuPu7, BuPu8, BuPu9, 
@@ -122,6 +249,22 @@ brews=[
     YlGnBu3, YlGnBu4, YlGnBu5, YlGnBu6, YlGnBu7, YlGnBu8, YlGnBu9,
     YlOrBr3, YlOrBr4, YlOrBr5, YlOrBr6, YlOrBr7, YlOrBr8, YlOrBr9,
     YlOrRd3, YlOrRd4, YlOrRd5, YlOrRd6, YlOrRd7, YlOrRd8, YlOrRd9,
+    Blues3, Blues4, Blues5, Blues6, Blues7, Blues8, Blues9,
+    Greens3, Greens4, Greens5, Greens6, Greens7, Greens8, Greens9,
+    Greys3, Greys4, Greys5, Greys6, Greys7, Greys8, Greys9,
+    Oranges3, Oranges4, Oranges5, Oranges6, Oranges7, Oranges8, Oranges9,
+    Purples3, Purples4, Purples5, Purples6, Purples7, Purples8, Purples9,
+    Reds3, Reds4, Reds5, Reds6, Reds7, Reds8, Reds9, 
+
+    Accent3, Accent4, Accent5, Accent6, Accent7, Accent8, 
+    Dark2_3, Dark2_4, Dark2_5, Dark2_6, Dark2_7, Dark2_8, 
+    Paired3, Paired4, Paired5, Paired6, Paired7, Paired8, Paired9, Paired10, Paired11,
+    Pastel1_3, Pastel1_4, Pastel1_5, Pastel1_6, Pastel1_7, Pastel1_8, Pastel1_9,
+    Pastel2_3, Pastel2_4, Pastel2_5, Pastel2_6, Pastel2_7, Pastel2_8,
+    Set1_3, Set1_4, Set1_5, Set1_6, Set1_7, Set1_8, Set1_9, 
+    Set2_3, Set2_4, Set2_5, Set2_6, Set2_7, Set2_8, 
+    Set3_3, Set3_4, Set3_5, Set3_6, Set3_7, Set3_8, Set3_9, Set3_10, Set3_11, Set3_12,
+
 ] 
 
 class BrewSelector:
--- tests.py
+++ tests.py
@@ -17,11 +17,14 @@ class SelectorTest(unittest.TestCase):
 	def test_get_brew(self):
 		self.assertEqual(self.bs.get_brew('sequential', 9, 'BuGn'), self.brew_9)
 
-	def test_list_schemes(self):
-		self.assertEqual(sorted(self.bs.list_schemes('sequential', 9)), sorted(('BuGn', 'BuPu', 'GnBu','OrRd', 'PuBu', 'PuBuGn', 'PuRd', 'RdPu', 'YlGn', 'YlGnBu', 'YlOrBr', 'YlOrRd')))
+	def test_list_schemes_sequential(self):
+		self.assertEqual(sorted(self.bs.list_schemes('sequential', 9)), sorted(('BuGn', 'BuPu', 'GnBu','OrRd', 'PuBu', 'PuBuGn', 'PuRd', 'RdPu', 'YlGn', 'YlGnBu', 'YlOrBr', 'YlOrRd', 'Blues', 'Greens', 'Greys', 'Oranges', 'Purples', 'Reds')))
 	
+	def test_list_schemes_qualitative(self):
+		self.assertEqual(sorted(self.bs.list_schemes('qualitative', 5)), sorted(('Accent', 'Dark2', 'Paired', 'Pastel1', 'Pastel2', 'Set1', 'Set2', 'Set3')))
+
 	def test_get_colorblind_safe(self):
-		raise Exception('write this test')
+		self.assertEqual(sorted(self.bs.list_schemes('qualitative', 3, colorblind_safe=True)), sorted(('Dark2', 'Paired', 'Set2')))
 
 	def test_get_photocopy_safe(self):
 		schemes=self.bs.list_schemes('sequential', 4, photocopy_safe=True)
@@ -29,7 +32,7 @@ class SelectorTest(unittest.TestCase):
 
 	def test_get_print_safe(self):
 		schemes=self.bs.list_schemes('sequential', 5, print_safe=True)
-		self.assertEqual(sorted(schemes), ('GnBu','PuRd', 'YlGnBu'))
+		self.assertEqual(sorted(schemes), sorted(('GnBu','PuRd', 'YlGnBu')))
 
 
 class BrewTest(unittest.TestCase):
